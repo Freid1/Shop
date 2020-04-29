e300cfd7646f7ef50e93d6b31d3e441747aeeb60
Dmitry <dtraider@mail.ru>
2020-04-29 18:19:53 +0300

    test 2

--- src/main/java/ru/dilmar/Start.java
+++ src/main/java/ru/dilmar/Start.java
@@ -1,11 +1,8 @@
 package ru.dilmar;
 
 
-<<<<<<< HEAD
-=======
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
->>>>>>> bf4d83a9a07dfb3a57a09e5f10fae603ed63e8d3
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,17 +15,13 @@ import ru.dilmar.repository.UsersRepository;
 //@EnableWebMvc
 //@EnableJpaRepositories
 public class Start {
-<<<<<<< HEAD
-
-    public static void main(String[] args) {
-        SpringApplication.run(Start.class, args);
-=======
     private static final Logger logger = LoggerFactory.getLogger(Start.class);
+
+
     public static void main(String[] args) {
         logger.info("Project ready to start");
         SpringApplication.run(Start.class, args);
 
->>>>>>> bf4d83a9a07dfb3a57a09e5f10fae603ed63e8d3
     }
 
 }
